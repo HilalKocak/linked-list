9e8416d17363e006fe4ab78bf7e93c3a9217936c
Hilal38 <hhilalkocak@gmail.com>
2024-03-23 22:58:21 +0300

    Initialieze project

---
+++ linked-list.js
@@ -0,0 +1,33 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+// const linkedListValues = (head) => {
+//   let array = []
+  
+//   let current = head
+//   while(current !== null){
+//     array.push(current.val)
+//     current = current.next
+//   }
+  
+//   return array
+// };
+
+const linkedListValues = (head) => {
+  
+    let values = []
+    collectValues(head, values)
+    return values
+    
+    function collectValues(node, values){
+      if(node === null) return values
+      values.push(node.val)
+      collectValues(node.next, values)
+    }
+   
+  };
+  
