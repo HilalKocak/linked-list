9c1176eb9a0d4aa6a3c7baea3dc65e6624183302
Hilal38 <hhilalkocak@gmail.com>
2024-03-26 07:57:07 +0300

    Implement delete node

---
+++ delete.js
@@ -0,0 +1,44 @@
+// const removeNode = (head, targetVal) => {
+//   if(head.val === targetVal) return head.next
+//   let current = head
+//   let prev = null
+//   while(current !== null){
+//     if(current.val === targetVal){
+//       prev.next = current.next
+//       break;
+//     }
+    
+//     prev = current
+//     current = current.next
+//   }
+  
+//   return head
+// };
+
+
+const removeNode = (head, targetVal) => {
+    if(head === null) return null
+    if(head.val === targetVal) return head.next
+    head.next = removeNode(head.next, targetVal)
+    return head
+  
+  };
+  
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+e.next = f;
+
+// a -> b -> c -> d -> e -> f
+
+console.log(removeNode(a, "c"))
+// a -> b -> d -> e -> f
