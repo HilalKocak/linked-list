6c44308c81d1b026038e200c91decb4c45eead6b
Hilal38 <hhilalkocak@gmail.com>
2024-03-26 10:04:58 +0300

    Fix time and space complexity

--- create.js
+++ create.js
@@ -16,12 +16,22 @@ class Node {
     return newNode.next
   };
 
-//Time O(n) Space O(1)
+//Time O(n) Space O(n^2)
 const createLinkedListRecursive = (values) => {
     if(values.length === 0) return null
     const head = new Node(values[0])
     head.next = createLinkedList(values.slice(1))
     return head
   };
-  
+
+
+
+  //Time O(n) Space O(n)
+const createLinkedListRecursiveRefactored = (values, index) => {
+    if(values.length === index) return null
+    const head = new Node(values[0])
+    head.next = createLinkedList(values, index)
+    return head
+  };
+
 console.log(createLinkedList(["h", "e", "y"]))
