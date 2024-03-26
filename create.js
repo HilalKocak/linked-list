03417845b8d7f1bda290b9830fc68417fec708bc
Hilal38 <hhilalkocak@gmail.com>
2024-03-26 09:54:33 +0300

    Create linked list from array

---
+++ create.js
@@ -0,0 +1,27 @@
+class Node {
+    constructor(val) {
+      this.val = val;
+      this.next = null;
+    }
+  }
+  //Time O(n) Space O(n)
+  const createLinkedList = (values) => {
+    let newNode = new Node(null)
+    let current = newNode
+    for(let value of values){
+      current.next = new Node(value)
+      current = current.next
+    }
+    
+    return newNode.next
+  };
+
+//Time O(n) Space O(1)
+const createLinkedListRecursive = (values) => {
+    if(values.length === 0) return null
+    const head = new Node(values[0])
+    head.next = createLinkedList(values.slice(1))
+    return head
+  };
+  
+console.log(createLinkedList(["h", "e", "y"]))
--- insert.js
+++ insert.js
@@ -24,7 +24,7 @@ const insertNode = (head, value, index) => {
   
   
 const insertNode2 = (head, value, index, count=0) => {
-    if(head === null) return
+    if(head === null) return null
     if(index === 0){
         let newNode = new Node(value)
         newNode.next = head
@@ -45,4 +45,15 @@ const insertNode2 = (head, value, index, count=0) => {
   // a -> b -> x -> c ->  d
   //     cur     newNext
   
-  
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+// a -> b -> c -> d
+
+console.log(insertNode(a, 'x', 2))
