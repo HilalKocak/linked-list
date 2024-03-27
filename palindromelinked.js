62146c7742c280a9919c7066f38e34e79de6af16
Hilal38 <hhilalkocak@gmail.com>
2024-03-28 00:30:43 +0300

    Palindrome linked list

---
+++ palindromelinked.js
@@ -0,0 +1,49 @@
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val);
+    this.next = (next === undefined ? null : next);
+}
+var isPalindrome = function(head) {
+    if(!head || !head.next) return true
+
+
+    let slow = head
+    let fast = head
+
+    while(fast.next && fast.next.next)
+    {
+        slow = slow.next
+        fast = fast.next.next
+    }
+
+    let second = slow.next
+    slow.next = null
+    let prev = null
+
+    while(second){
+        let next = second.next
+        second.next = prev
+        prev = second
+        second = next
+    }
+
+    // prev = head of reversed second half
+    let secondHalf = prev
+    let first = head
+
+    while(secondHalf && first){
+        if(secondHalf.val !== first.val){
+            return false
+        }
+        secondHalf = secondHalf.next
+        first = first.next
+    }
+    return true
+
+};
+
+let head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(2);
+head.next.next.next = new ListNode(1);
+
+console.log(isPalindrome(head));
